1364c930a80d290ff9dd0422551379bac8df8cd0
A.S.Kozienko <a.s.kozienko@gmail.com>
2012-07-05 13:55:30 +0300

    remove dependency on FrameworkExtraBundle

--- Controller/SwfuploadController.php
+++ Controller/SwfuploadController.php
@@ -3,7 +3,6 @@ namespace Fp\SwfuploadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Form;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration as Extra;
 
 class SwfuploadController extends Controller
 {
@@ -11,14 +10,13 @@ class SwfuploadController extends Controller
      * @param mixed     $data       The initial data for the form
      * @param array     $options    Options for the form
      *
-     * @Extra\Template(engine="twig")
      */
     public function getFormAction(array $options, $data = null)
     {
         $form = $this->createForm('swfupload_file', $data, $options);
 
-        return array(
-            'form' => $form->createView()
-        );
+        $this->renderView('FpSwfuploadBundle:Swfupload:getForm.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
